b489a7961c0a6939be83bcc82919ad9a75c5448d
ssokov <sokovykh.work@gmail.com>
2025-10-18 20:40:20 +0300

    init db

--- docs/tgbot-log-hub.sql
+++ docs/tgbot-log-hub.sql
@@ -1,131 +1,127 @@
 ﻿-- =============================================================================
 -- Diagram Name: tgbot-log-hub
--- Created on: 6/8/2022 12:02:23 PM
+-- Created on: 18.10.2025 20:32:28
 -- Diagram Version: 
 -- =============================================================================
 
-CREATE TABLE "statuses" (
-	"statusId" SERIAL NOT NULL,
-	"title" varchar(255) NOT NULL,
-	"alias" varchar(64) NOT NULL,
-	CONSTRAINT "statuses_pkey" PRIMARY KEY("statusId"),
-	CONSTRAINT "statuses_alias_key" UNIQUE("alias")
+CREATE TABLE "admin_role" (
+	"id" SERIAL NOT NULL,
+	"role_name" varchar(255) NOT NULL,
+	"created_at" timestamp with time zone NOT NULL DEFAULT now(),
+	PRIMARY KEY("id")
 );
 
-CREATE TABLE "users" (
-	"userId" SERIAL NOT NULL,
-	"login" varchar(64) NOT NULL,
-	"password" varchar(64) NOT NULL,
-	"authKey" varchar(32),
+CREATE TABLE "admin" (
+	"id" BIGSERIAL NOT NULL,
+	"login" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password_hash" varchar(255) NOT NULL,
+	"role_id" int4 NOT NULL,
+	"status" int4 NOT NULL,
 	"createdAt" timestamp with time zone NOT NULL DEFAULT now(),
-	"lastActivityAt" timestamp with time zone,
-	"statusId" int4 NOT NULL,
-	CONSTRAINT "users_pkey" PRIMARY KEY("userId")
+	PRIMARY KEY("id"),
+	CONSTRAINT "uk_admin_login" UNIQUE("login"),
+	CONSTRAINT "uk_admin_email" UNIQUE("email")
 );
 
-CREATE INDEX "IX_FK_users_statusId_users" ON "users" USING BTREE (
-	"statusId"
+CREATE TABLE "service_admin" (
+	"service_id" int8 NOT NULL,
+	"admin_id" int8 NOT NULL,
+	"assignedAt" timestamp with time zone NOT NULL DEFAULT now(),
+	PRIMARY KEY("service_id","admin_id")
 );
 
-
-CREATE TABLE "vfsFiles" (
-	"fileId" SERIAL NOT NULL,
-	"folderId" int4 NOT NULL,
-	"title" varchar(255) NOT NULL,
-	"path" varchar(255) NOT NULL,
-	"params" text,
-	"isFavorite" bool DEFAULT false,
-	"mimeType" varchar(255) NOT NULL,
-	"fileSize" int4 DEFAULT 0,
-	"fileExists" bool NOT NULL DEFAULT true,
-	"createdAt" timestamp NOT NULL DEFAULT now(),
-	"statusId" int4 NOT NULL,
-	CONSTRAINT "vfsFiles_pkey" PRIMARY KEY("fileId")
-);
-
-CREATE INDEX "IX_FK_vfsFiles_folderId_vfsFiles" ON "vfsFiles" USING BTREE (
-	"folderId"
-);
-
-
-CREATE INDEX "IX_FK_vfsFiles_statusId_vfsFiles" ON "vfsFiles" USING BTREE (
-	"statusId"
-);
-
-
-CREATE TABLE "vfsFolders" (
-	"folderId" SERIAL NOT NULL,
-	"parentFolderId" int4,
-	"title" varchar(255) NOT NULL,
-	"isFavorite" bool DEFAULT false,
-	"createdAt" timestamp NOT NULL DEFAULT now(),
-	"statusId" int4 NOT NULL,
-	CONSTRAINT "vfsFolders_pkey" PRIMARY KEY("folderId")
-);
-
-CREATE INDEX "IX_FK_vfsFolders_folderId_vfsFolders" ON "vfsFolders" USING BTREE (
-	"parentFolderId"
-);
-
-
-CREATE INDEX "IX_FK_vfsFolders_statusId_vfsFolders" ON "vfsFolders" USING BTREE (
-	"statusId"
-);
-
-
-CREATE TABLE "vfsHashes" (
-	"hash" varchar(40) NOT NULL,
-	"namespace" varchar(32) NOT NULL,
-	"extension" varchar(4) NOT NULL,
-	"fileSize" int4 NOT NULL DEFAULT 0,
-	"width" int4 NOT NULL DEFAULT 0,
-	"height" int4 NOT NULL DEFAULT 0,
-	"blurhash" text,
-	"error" text,
+CREATE TABLE "service" (
+	"id" BIGSERIAL NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"type_id" int4,
+	"api_key" varchar(255) NOT NULL,
+	"status" int4 NOT NULL,
 	"createdAt" timestamp with time zone NOT NULL DEFAULT now(),
-	"indexedAt" timestamp with time zone,
-	CONSTRAINT "vfsHashes_pkey" PRIMARY KEY("hash","namespace")
+	PRIMARY KEY("id")
 );
 
-CREATE INDEX "IX_vfsHashes_indexedAt" ON "vfsHashes" USING BTREE (
-	"indexedAt"
+CREATE TABLE "service_type" (
+	"id" SERIAL NOT NULL,
+	"type_name" varchar(255) NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "service_user" (
+	"id" BIGSERIAL NOT NULL,
+	"tg_id" int8 NOT NULL DEFAULT -1,
+	"nickname" varchar(255) NOT NULL,
+	"params" jsonb,
+	"createdAt" timestamp with time zone NOT NULL DEFAULT now(),
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "service_log" (
+	"id" BIGSERIAL NOT NULL,
+	"type_id" int8 NOT NULL,
+	"error_code" int4,
+	"message" text,
+	"service_id" int8 NOT NULL,
+	"user_id" int8,
+	"additional_data" jsonb,
+	"createdAt" timestamp with time zone NOT NULL DEFAULT now(),
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "log_type" (
+	"id" BIGSERIAL NOT NULL,
+	"type_name" varchar(255) NOT NULL,
+	PRIMARY KEY("id")
 );
 
 
-
-ALTER TABLE "users" ADD CONSTRAINT "FK_users_statusId" FOREIGN KEY ("statusId")
-	REFERENCES "statuses"("statusId")
+ALTER TABLE "admin" ADD CONSTRAINT "Ref_admin_to_admin_role" FOREIGN KEY ("role_id")
+	REFERENCES "admin_role"("id")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
-ALTER TABLE "vfsFiles" ADD CONSTRAINT "vfsFiles_folderId_fkey" FOREIGN KEY ("folderId")
-	REFERENCES "vfsFolders"("folderId")
+ALTER TABLE "service_admin" ADD CONSTRAINT "Ref_service_admin_to_service" FOREIGN KEY ("service_id")
+	REFERENCES "service"("id")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
-ALTER TABLE "vfsFiles" ADD CONSTRAINT "vfsFiles_statusId_fkey" FOREIGN KEY ("statusId")
-	REFERENCES "statuses"("statusId")
+ALTER TABLE "service_admin" ADD CONSTRAINT "Ref_service_admin_to_admin" FOREIGN KEY ("admin_id")
+	REFERENCES "admin"("id")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
-ALTER TABLE "vfsFolders" ADD CONSTRAINT "vfsFolders_parentFolderId_fkey" FOREIGN KEY ("parentFolderId")
-	REFERENCES "vfsFolders"("folderId")
+ALTER TABLE "service" ADD CONSTRAINT "Ref_service_to_service_type" FOREIGN KEY ("type_id")
+	REFERENCES "service_type"("id")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
-ALTER TABLE "vfsFolders" ADD CONSTRAINT "vfsFolders_statusId_fkey" FOREIGN KEY ("statusId")
-	REFERENCES "statuses"("statusId")
+ALTER TABLE "service_log" ADD CONSTRAINT "Ref_service_log_to_service_user" FOREIGN KEY ("user_id")
+	REFERENCES "service_user"("id")
 	MATCH SIMPLE
-	ON DELETE RESTRICT
-	ON UPDATE RESTRICT
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "service_log" ADD CONSTRAINT "Ref_service_log_to_service" FOREIGN KEY ("service_id")
+	REFERENCES "service"("id")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+	NOT DEFERRABLE;
+
+ALTER TABLE "service_log" ADD CONSTRAINT "Ref_service_log_to_log_type" FOREIGN KEY ("type_id")
+	REFERENCES "log_type"("id")
+	MATCH SIMPLE
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 	NOT DEFERRABLE;
 
 
